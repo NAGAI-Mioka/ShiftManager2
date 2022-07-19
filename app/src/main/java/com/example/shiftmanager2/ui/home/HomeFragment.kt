1801901483e73f51577f2109dcd2be25214d254d
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2022-07-19 14:06:05 +0900

    again again

--- app/src/main/java/com/example/shiftmanager2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/shiftmanager2/ui/home/HomeFragment.kt
@@ -1,13 +1,22 @@
 package com.example.shiftmanager2.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.shiftmanager2.databinding.FragmentHomeBinding
+import com.kizitonwose.calendarview.CalendarView
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.model.CalendarMonth
+import com.kizitonwose.calendarview.model.DayOwner
+import com.kizitonwose.calendarview.ui.DayBinder
+import com.kizitonwose.calendarview.ui.MonthHeaderFooterBinder
+import java.time.DayOfWeek
+import java.time.YearMonth
+import java.time.temporal.WeekFields
+import java.util.*
 
 class HomeFragment : Fragment() {
 
@@ -22,16 +31,55 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val calendarView: CalendarView = binding.calendarView
+        calendarView.dayBinder = object : DayBinder<DayViewContainer> {
+            // Called only when a new container is needed.
+            override fun create(view: View) = DayViewContainer(view)
+
+            // Called every time we need to reuse a container.
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                // この中で日ごとの表示を設定する
+                container.textView.text = day.date.dayOfMonth.toString()
+                container.textView.setTextColor(Color.BLACK)
+                if (day.date.dayOfWeek == DayOfWeek.SUNDAY) {
+                    container.textView.setTextColor(Color.RED)
+                }
+                if (day.date.dayOfWeek == DayOfWeek.SATURDAY) {
+                    container.textView.setTextColor(Color.BLUE)
+                }
+                if (day.owner != DayOwner.THIS_MONTH) {
+                    when (day.date.dayOfWeek) {
+                        DayOfWeek.SUNDAY -> {
+                            container.textView.setTextColor(Color.rgb(240, 170, 170))
+                        }
+                        DayOfWeek.SATURDAY -> {
+                            container.textView.setTextColor(Color.rgb(140, 140, 255))
+                        }
+                        else -> {
+                            container.textView.setTextColor(Color.GRAY)
+                        }
+                    }
+                }
+            }
         }
+        calendarView.monthHeaderBinder = object : MonthHeaderFooterBinder<MonthViewContainer> {
+            override fun create(view: View) = MonthViewContainer(view)
+            override fun bind(container: MonthViewContainer, month: CalendarMonth) {
+                container.textView.text = "${
+                    month.yearMonth.month.name.lowercase(Locale.getDefault())
+                        .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+                } ${month.year}"
+            }
+        }
+        val currentMonth = YearMonth.now()
+        val lastMonth = currentMonth.plusMonths(5)
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+        calendarView.setup(currentMonth, lastMonth, firstDayOfWeek)
+        calendarView.scrollToMonth(currentMonth)
+
         return root
     }
 
